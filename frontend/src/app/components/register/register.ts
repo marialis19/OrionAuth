c2323e3e50fc23d6fe261a46f364d685661f7c03
Lis <168233558+marialis19@users.noreply.github.com>
2026-05-03 20:10:56 -0300

    feat: modified:   frontend/src/app/components/register/register.ts

--- frontend/src/app/components/register/register.ts
+++ frontend/src/app/components/register/register.ts
@@ -163,7 +163,7 @@ export class Register {
       next: () => {
         this.isLoading = false;
         this.successMessage = '¡Cuenta creada! Redirigiendo al login...';
-        setTimeout(() => this.router.navigate(['/login']), 2500);
+        setTimeout(() => this.router.navigate(['/login']), 2000);
       },
       error: (err) => {
         this.isLoading = false;
